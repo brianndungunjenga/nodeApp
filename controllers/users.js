8c8138b7741bdf8ff67af349484ffde404bf599e
brianndungunjenga <ndungunjenga96@gmail.com>
2019-10-23 05:57:12 +0300

    Added new variables

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 module.exports.controller = (app) => {
     // get users page
     app.get('/users', (req, res) => {
-        res.render('index', {title: 'Users'});
+        res.render('users', {title: 'Users', description: 'This is the description to all the users', extra: 'Thank you for being an esteemed user', king: 'Node is king!' });
     })
 }
